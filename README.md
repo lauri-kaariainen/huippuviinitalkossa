a6b0f36d14b2632b874486cd23730591d871ca3a
Lauri Kääriäinen <4522622+lauri-kaariainen@users.noreply.github.com>
2020-04-09 01:17:10 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# huippuviinitalkossa
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,108 @@
+import { render, Fragment } from "preact";
+import {
+  useReducer,
+  useState,
+  useCallback,
+  useRef,
+  useEffect
+} from "preact/hooks";
+import "./style/style.scss";
+import fiveStarWines from "./konala5starwines.json";
+import fourStarWines from "./konala4starwines.json";
+
+const F = Fragment;
+
+const NameButton = ({ id, name }) => (
+  <button
+    class="wineName"
+    onclick={() => (window.location = "https://alko.fi/tuotteet/" + id)}
+  >
+    {name}
+  </button>
+);
+
+const Wine = ({ wine, starAmount }) => (
+  <div
+    className={
+      (starAmount === 5 ? "fiveStars" : "fourStars") + " wine " + wine[5]
+    }
+    key={wine[0]}
+  >
+    <NameButton name={wine[1]} id={wine[0]} />
+    <br />
+    <span className={wine[5]} />
+    {starAmount !== 5 ? <F /> : <span class="fiveStarsSpan" />}
+    <span class="price">{wine[3]}€</span>
+    {wine[6] + " " + wine[7]}
+  </div>
+);
+
+function Wines() {
+  const [orderByPrice, setOrderByPrice] = useState(false);
+  const [filterText, setFilterText] = useState("");
+
+  const wineFilter = (filterText, wine) =>
+    filterText.length
+      ? wine[1].toLowerCase().includes(filterText.toLowerCase()) ||
+        wine[6].toLowerCase().includes(filterText.toLowerCase()) ||
+        wine[7].toLowerCase().includes(filterText.toLowerCase())
+      : true;
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `5 Star Wines`;
+  });
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={filterText}
+        oninput={ev => {
+          setFilterText(ev.target.value);
+        }}
+        placeholder="filter"
+      />
+      <span>Järjestä:</span>
+      <button
+        className={"filterButton" + (orderByPrice ? " active" : "")}
+        onclick={_ => setOrderByPrice(!orderByPrice)}
+      >
+        €
+      </button>
+      {(orderByPrice
+        ? fiveStarWines
+            .slice()
+            .sort((a, b) => parseFloat(a[4]) - parseFloat(b[4]))
+        : fiveStarWines
+      )
+        .filter(wineFilter.bind(null, filterText))
+        .map(wine => (
+          <Wine starAmount={5} wine={wine} />
+        ))}
+      {(orderByPrice
+        ? fourStarWines
+            .slice()
+            .sort((a, b) => parseFloat(a[4]) - parseFloat(b[4]))
+        : fourStarWines
+      )
+        .filter(wineFilter.bind(null, filterText))
+        .map(wine => (
+          <Wine starAmount={4} wine={wine} />
+        ))}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <F>
+      <h1>Huippuviinit Konalassa</h1>
+      <Wines />
+    </F>
+  );
+}
+
+if (typeof window !== "undefined") {
+  render(<App />, document.getElementById("root"));
+}
